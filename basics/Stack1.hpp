854593063914fe53bcc4009260dd8446c015477e
Eden <3397436863@qq.com>
2025-12-27 13:38:20 +0800

    定义了Stack的构造函数与析构函数

--- basics/Stack1.hpp
+++ basics/Stack1.hpp
@@ -6,6 +6,16 @@ class Stack{
 private:
   std::vector<T> elems  ;
 public:
+  Stack() = default;
+
+  //在只需要类名而非类型名的地方。 请只使用类名
+  Stack(Stack const& other);
+  Stack& operator=(Stack const& other);
+  /* 即使下面的声明与上面使等价的
+  Stack(Stack<T> const& other);
+  Stack<T>& operator=(Stack<T> const& other);
+  */
+ ~Stack() = default;
   void Push(T elem);
   void Pop();
   T const& Top() const;
@@ -26,4 +36,15 @@ template <typename T>
 T const& Stack<T>::Top()const{
     assert(!Empty());
     return elems.back();
+}
+
+template<typename T>
+Stack<T>::Stack(Stack const& other){
+    elems.emplace_back(other.elems);
+}
+
+template<typename T>
+Stack<T>& Stack<T>::operator=(Stack const& other){
+    elems = other.elems;
+    return *this;
 }
