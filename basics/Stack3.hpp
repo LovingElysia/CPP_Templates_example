2feea3125dfae15135ea5185b66d56708ed8c412
Eden <3397436863@qq.com>
2025-12-27 14:40:22 +0800

    feat: 新增Stack可选底层容器版本，提供默认模板实参

---
+++ basics/Stack3.hpp
@@ -0,0 +1,66 @@
+#include<vector>
+#include<cassert>
+#include<ostream>
+
+template<typename T,typename Cont = std::vector<T>>
+class Stack;
+template<typename T>
+std::ostream& operator<<(std::ofstream&os,Stack<T> const& stack);
+
+template <typename T,typename Cont = std::vector<T>>
+class Stack{
+private:
+  Cont elems  ;
+public:
+  Stack() = default;
+
+  //在只需要类名而非类型名的地方。 请只使用类名
+  Stack(Stack const& other);
+  Stack& operator=(Stack const& other);
+  /* 即使下面的声明与上面使等价的
+  Stack(Stack<T> const& other);
+  Stack<T>& operator=(Stack<T> const& other);
+  */
+ ~Stack() = default;
+  void Push(T elem);
+  void Pop();
+  T const& Top() const;
+  bool Empty() const;  
+  friend std::ostream& operator<<<T> (std::ostream&os,Stack<T> const& stack);
+};
+
+//每一个成员函数都必须含有这两个类型参数
+template <typename T,typename Cont>
+void Stack<T,Cont>::Push(T elem){
+    elems.emplace_back(elem);
+}
+
+
+template <typename T,typename Cont>
+void Stack<T,Cont>::Pop(){
+    assert(!Empty());
+    elems.pop_back();
+}
+
+
+template <typename T,typename Cont>
+T const& Stack<T,Cont>::Top()const{
+    assert(!Empty());
+    return elems.back();
+}
+
+template <typename T,typename Cont>
+Stack<T,Cont>::Stack(Stack const& other):elems(other.elems){}
+
+
+template <typename T,typename Cont>
+Stack<T,Cont>& Stack<T,Cont>::operator=(Stack const& other){
+    elems = other.elems;
+    return *this;
+}
+
+
+template <typename T,typename Cont>
+bool Stack<T,Cont>::Empty() const{
+    return elems.empty();
+}
